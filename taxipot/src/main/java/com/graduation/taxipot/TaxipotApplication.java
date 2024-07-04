d8424fa8d46ed8513ffee92f34e6ddfe8f54b43b
Moonjunwoong <117076854+junwoong2@users.noreply.github.com>
2024-07-04 15:13:45 +0900

    Add files via upload

---
+++ taxipot/src/main/java/com/graduation/taxipot/TaxipotApplication.java
@@ -0,0 +1,13 @@
+package com.graduation.taxipot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxipotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxipotApplication.class, args);
+	}
+
+}
