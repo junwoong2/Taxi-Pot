0fa204b72614eb3dfc87a32b2e47c76b6debb461
leejunjae00 <wnswo8633@naver.com>
2024-08-05 13:24:41 +0900

    로그인 후 게시판 메인 생성 board 관련 생성

---
+++ taxipot/src/main/java/com/graduation/taxipot/board/entity/Board.java
@@ -0,0 +1,45 @@
+package com.graduation.taxipot.board.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "board")
+public class Board {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String content;
+
+  @Column(nullable = false)
+  private String author;
+
+  @Column(nullable = false)
+  private LocalDateTime createdDate;
+
+  @Column(nullable = false)
+  private LocalDateTime modifiedDate;
+
+  @PrePersist
+  public void prePersist() {
+    createdDate = LocalDateTime.now();
+    modifiedDate = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    modifiedDate = LocalDateTime.now();
+  }
+}
